6972fd2578baef0c884b8f3f26b30bcbdb5f950c
Saad Abdul Hamid Parkar <saadparkar4@gmail.com>
2025-05-05 18:48:29 +0300

    “Completed”

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
+import products from "@/data/products";
 import { Image, Text, View } from "react-native";
 
 export default function Index() {
+	let storeProducts = products[0];
 	return (
 		<View
 			style={{
@@ -10,7 +12,7 @@ export default function Index() {
 			}}>
 			<Text
 				style={{
-					margin: 20,
+					margin: 10,
 				}}>
 				<h1> My React Native Shop</h1>
 			</Text>
@@ -24,11 +26,84 @@ export default function Index() {
 			<Image
 				source={{ uri: "https://i.giphy.com/KX5nwoDX97AtPvKBF6.webp" }}
 				style={{
-					width: 500,
-					height: 500,
+					width: 300,
+					height: 300,
 					margin: 20,
+					borderRadius: 10,
 				}}
 			/>
+			<View
+				style={{
+					display: "flex",
+					flex: 1,
+					flexDirection: "row",
+					justifyContent: "center",
+					alignItems: "flex-start",
+					width: "100%",
+					alignContent: "center",
+				}}>
+				<View
+					style={{
+						padding: 10,
+						display: "flex",
+						alignItems: "center",
+
+						justifyContent: "center",
+					}}>
+					<Image
+						source={{ uri: "https://i.giphy.com/KX5nwoDX97AtPvKBF6.webp" }}
+						style={{
+							width: 100,
+							height: 100,
+							margin: 20,
+							borderRadius: 10,
+						}}
+					/>
+					<Text
+						style={{
+							margin: 10,
+						}}>
+						{storeProducts.pName}
+					</Text>
+					<Text
+						style={{
+							margin: 10,
+						}}>
+						{storeProducts.pPrice} KWD
+					</Text>
+				</View>
+				<View
+					style={{
+						padding: 10,
+						display: "flex",
+						alignItems: "center",
+
+						justifyContent: "center",
+					}}>
+					<Image
+						source={{ uri: "https://i.giphy.com/KX5nwoDX97AtPvKBF6.webp" }}
+						style={{
+							width: 100,
+							height: 100,
+							margin: 20,
+							borderRadius: 10,
+						}}
+					/>
+					<Text
+						style={{
+							margin: 10,
+						}}>
+						{storeProducts.pName}
+					</Text>
+					<Text
+						style={{
+							margin: 10,
+						}}>
+						{storeProducts.pPrice} KWD
+					</Text>
+				</View>
+			</View>
+			<View />
 		</View>
 	);
 }
---
+++ data/products.ts
@@ -0,0 +1,33 @@
+interface product {
+	pName: string;
+	pImage: string;
+	pPrice: number;
+}
+
+const products: product[] = [
+	{
+		pName: "Mobile Stand",
+		pImage: "https://i.giphy.com/KX5nwoDX97AtPvKBF6.webp",
+		pPrice: 10,
+	},
+
+	{
+		pName: "Tablet Stand",
+		pImage: "https://i.giphy.com/KX5nwoDX97AtPvKBF6.webp",
+		pPrice: 20,
+	},
+
+	{
+		pName: "Laptop Stand",
+		pImage: "https://i.giphy.com/KX5nwoDX97AtPvKBF6.webp",
+		pPrice: 30,
+	},
+
+	{
+		pName: "TV Stand",
+		pImage: "https://i.giphy.com/KX5nwoDX97AtPvKBF6.webp",
+		pPrice: 40,
+	},
+];
+
+export default products;
