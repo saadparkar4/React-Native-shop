2a933c514537af0a6af945456698c877992fd66e
Saad Abdul Hamid Parkar <saadparkar4@gmail.com>
2025-05-05 19:40:18 +0300

    “Completed”

--- components/ProductItems.tsx
+++ components/ProductItems.tsx
@@ -8,26 +8,24 @@ const ProductItems = () => {
 		<View
 			style={{
 				display: "flex",
-				flex: 1,
+				// flex: 1, -- This will squeeze everything inside one single div or section in the view area
 				flexDirection: "row",
 				justifyContent: "center",
 				alignItems: "flex-start",
 				width: "100%",
-				alignContent: "center",
 			}}>
 			<View
 				style={{
 					padding: 10,
 					display: "flex",
 					alignItems: "center",
-
 					justifyContent: "center",
 				}}>
 				<Image
 					source={{ uri: "https://i.giphy.com/KX5nwoDX97AtPvKBF6.webp" }}
 					style={{
-						width: 100,
-						height: 100,
+						width: 140,
+						height: 140,
 						margin: 20,
 						borderRadius: 10,
 					}}
@@ -50,14 +48,13 @@ const ProductItems = () => {
 					padding: 10,
 					display: "flex",
 					alignItems: "center",
-
 					justifyContent: "center",
 				}}>
 				<Image
 					source={{ uri: "https://i.giphy.com/KX5nwoDX97AtPvKBF6.webp" }}
 					style={{
-						width: 100,
-						height: 100,
+						width: 140,
+						height: 140,
 						margin: 20,
 						borderRadius: 10,
 					}}
